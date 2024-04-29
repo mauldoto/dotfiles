4740826c4b65d620205fe67128d504a4d2bb6dbe
mauldoto <maulanaly97@gmail.com>
2024-04-29 15:34:37 +0700

    add nvim dotfiles and gitigonre

---
+++ nvim/lua/colorschema.lua
@@ -0,0 +1,8 @@
+-- colorscheme config: kanagawa
+local themeStatus, kanagawa = pcall(require, "kanagawa")
+
+if themeStatus then
+    vim.cmd("colorscheme kanagawa")
+else
+    return
+end
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,16 @@
+-- 📂lua/🌑plugins.lua
+
+-- Automatically run: PackerCompile
+vim.api.nvim_create_autocmd("BufWritePost", {
+    group = vim.api.nvim_create_augroup("PACKER", { clear = true }),
+    pattern = "plugins.lua",
+    command = "source <afile> | PackerCompile",
+})
+
+return require("packer").startup(function(use)
+    -- Packer
+    use("wbthomason/packer.nvim")
+    
+    -- Colorschema
+    use("rebelot/kanagawa.nvim")
+end)
